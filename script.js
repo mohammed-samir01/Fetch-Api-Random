78a111fa4e869b69d26e3da3e4a48acc97236d22
mohamed_samir <86778943+mohammed-samir01@users.noreply.github.com>
2022-06-05 03:21:43 +0200

    add

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const cat_result  = document.getElementById('cat_result');
+const dog_result  = document.getElementById('dog_result');
+const cat_btn     = document.getElementById('cat_btn');
+const dog_btn     = document.getElementById('dog_btn');
+
+
+
+
+cat_btn.addEventListener('click',getRandomCat);
+dog_btn.addEventListener('click',getRandomDog);
+
+    function getRandomCat() {
+        fetch("https://aws.random.cat/meow")
+
+            .then(function (response){
+
+                return response.json();
+            })
+
+            .then(function (data){
+
+                cat_result.innerHTML = `<img src=${data.file} alt="Cat" />`
+
+            })
+
+    }
+    function getRandomDog() {
+        fetch("https://random.dog/woof.json")
+
+            .then(function (response){
+
+                return response.json();
+            })
+
+            .then(function (data){
+
+                dog_result.innerHTML = `<img src=${data.url} alt="Dog" />`
+
+            })
+    }
